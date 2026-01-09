68e4a1a8d091ccf36147197f987cb966674d1bbc
Hariharan <hariharan9572@gmail.com>
2026-01-09 17:56:03 +0530

    model object

--- src/main/java/com/hariharan/SpringBootWeb1/HomeController.java
+++ src/main/java/com/hariharan/SpringBootWeb1/HomeController.java
@@ -3,6 +3,7 @@ package com.hariharan.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,19 +13,19 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
         /*int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));*/
         int result = num1+num2+1;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
-        return "result.jsp";
+        return "result";
     }
 
 }
